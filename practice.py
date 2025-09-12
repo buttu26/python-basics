692fbe4681b15f299ea946f73bcdf3f2324b704a
sadvik26 <msadvik26@gmail.com>
2025-09-12 11:07:28 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -73,5 +73,67 @@ if(year%4==0 and year%100!=0) or year%400==0:
     print("its a leap year")
 else:
     print("its not a leap year")
-    
+
+#practice questions
+# write a program to find the average of all numbers in a list
+ 
+list1=[1,2,3,4,5]
+l=len(list1)
+sum=sum(list1)
+print(sum/l)
+
+
+# count how many positive negative and zero values are in a list
+
+list2=[-2,-3,6,5,-8,4,0,10,0]
+list1=[]
+list3=[]
+list4=[]
+
+for num in list2:
+   if (num>0):
+     list1.append(num)
+print(list1)
+for num in list2:
+   if (num<0):
+      list3.append(num)
+for num in list2:
+   if (num==0):
+     list4.append(num)
+print(list3)
+positive_num=len(list1)
+negative_num=len(list3)
+zeroes_=len(list4)
+print(positive_num)
+print(negative_num)
+print(zeroes_)
+
+# remove duplicate elements from a list
+
+list1=[-2,-3,6,5,-8,4,0,10,0]
+list1.pop(-1)
+print(list1)
+
+
+# write a program to seperate even and odd numbers into two new lists
+
+list1=[1,2,3,4,5,6]
+list2=[]
+list3=[]
+for num in list1:
+   if (num%2==0):
+      list2.append(num)
+print(list2)
+for num in list1:
+   if (num%2!=0):
+      list3.append(num)
+print(list3)
+
+
+# take a list of names and print the longest name
+
+list1=["laxmi narasimha reddy","sadvik yadav","charan teja","sreekar reddy"] 
+list1.sort()
+print(list1)
+print(list1[1])
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
