4de2545138c13282e52621034d776bccc8d1807c
sadvik26 <msadvik26@gmail.com>
2025-09-16 12:00:33 +0530

    Update Numpy_Arrays.py

--- Numpy_Arrays.py
+++ Numpy_Arrays.py
@@ -4,4 +4,8 @@
 # -> Powerful array objects for our python program(more effective than python lists.)
 # -> Vectorized operations (Fast element wise calculations.)
 # -> Supports for linear algebra , statistics , Random numbers operations etc..
-# -> 
+
+#importing the numpy library.
+import numpy as np
+aa=np.array([1,2,3])
+print(aa)
