d1381635eb6e25aa6d08a4d56aa7cd98129979d7
sadvik26 <msadvik26@gmail.com>
2025-09-16 11:50:15 +0530

    Add files via upload

---
+++ Numpy_Arrays.py
@@ -0,0 +1,7 @@
+#Numpy:
+#Numpy (Numerical Python) is a python library used for scientific and mathematical computing.
+# It provides:
+# -> Powerful array objects for our python program(more effective than python lists.)
+# -> Vectorized operations (Fast element wise calculations.)
+# -> Supports for linear algebra , statistics , Random numbers operations etc..
+# -> 
