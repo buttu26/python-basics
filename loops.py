de5e41e28b04e2616fb685851d946c5d8543a496
sadvik26 <msadvik26@gmail.com>
2025-09-19 10:22:50 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,73 +1,70 @@
-# # 00
-
-
-
-
-
-
-
-
-# fruits=["apple","kiwi","mango"]
-# for fruit in fruits:
-#     print(fruit)
-
-# fruits="apple"
-# for i in fruits:
-#     print(i)
-
-    #5 table
-for i in range(1,11):     
-    print(5*i or i*5)
-for i in range(1,11):    # forprinting 5 table
-    print("5 *",i, "=",5*i)
-
-for i in range(1,11):    # forprinting 7 table
-    print("7 *",i, "=",7*i)
-
-
-
-
-
-
-
-
-
-
-    # loop is a program
+# loop is a program
 # for variable in seq:
 # code block
 # #range(start value(0),end value,step value)
-# for i in range(10):
-#     print("lucky")
+# # for i in range(10):
+# #     print("lucky")
 
-# for i in range(0,10):
-#     print(i)
+# # for i in range(0,10):
+# #     print(i)
 
-#  for i in range(0,10,1):
-#     print(i)
+# #  for i in range(0,10,1):
+# #     print(i)
 
-#   for i in range(0,10,2):
-#     print(i)
-#print the numbers from 9 to 27
-# for i in range(9,28,1):
-#     print(i)
-#using defalt values print the numbers upto 15>
-# for i in range(0,16,1):
-#     print(i)
+# #   for i in range(0,10,2):
+# #     print(i)
+# #print the numbers from 9 to 27
+# # for i in range(9,28,1):
+# #     print(i)
+# #using defalt values print the numbers upto 15>
+# # for i in range(0,16,1):
+# #     print(i)
  
-# for i in range(10,0,-1):
+# # for i in range(10,0,-1):
+# #     print(i)
+
+#     #print even numbers from 0 to 20
+# # for i in range(0,21,2):
+# #         print(i)
+
+#     #print squares of number upto 6
+# # for i in range(0,7,1):
+# #     print(i*i)
+
+
+# fruits=["apple","mango","orange"]
+# for fruit in fruits:
+#     print(fruit)
+
+# #5 table
+# for i in range(1,11):
+#     print("5 *",i,"=",5*i)
+
+# #7 table
+# for i in range(1,11):
+#     print("7 *",i,"=",7*i)
+
+    #1+2+3+4+5.....+25=?
+# sum=0
+# for i in range(1,26):
+#     sum=sum+i
+#     print(sum)
+
+# for i in range(1,10):
 #     print(i)
-
-    #print even numbers from 0 to 20
-# for i in range(0,21,2):
-#         print(i)
-
-    #print squares of number upto 6
-# for i in range(0,7,1):
-#     print(i*i)
+#     if i==7:
+#       break 
 
 
-fruits=["apple","mango","orange"]
-for fruit in fruits:
-    print(fruit)
+# for i in range(1,10):
+#     print(i)
+#     if i==7:
+#       continue
+    
+
+for i in range(1,10):
+    if i%2 ==5:
+       pass
+    else:
+        print(i)
 
